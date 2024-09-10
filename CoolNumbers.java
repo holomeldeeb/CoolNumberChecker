b27ad301bb97f9c93603b00c424f77aed50c9d8a
Mohamed Eldeeb <holom@Mohameds-MacBook-Air.local>
2024-09-10 21:30:42 +0200

    feature signup working

--- CoolNumbers.java
+++ CoolNumbers.java
@@ -1,1 +1,40 @@
-
+public class CoolNumbers {
+    public static void main(String[] args){
+        System.out.println(isCool(0, 49));
+        System.out.println(countCools(3 , 1111 ,9999));
+    
+        
+    }
+    public static boolean isCool (int x , int n){
+        int temp = x;
+        boolean cool= false ;
+        while (temp > 0){
+            int digit = temp % 10 ;
+            if (digit ==0 || n % digit !=0){
+                cool = false;
+                break;
+            }
+            else {
+                cool = true;
+            }
+            temp /=10;
+        }
+        return cool;
+    
+        
+    }
+    public static int countCools (int n , int a , int b){
+        int count = 0;
+        for (int i = a; i <= b; i++) {
+            if (isCool(i,n)){
+               ++count;
+            }
+            
+        }
+        return count;
+        
+    
+    
+        }
+    }
+    
